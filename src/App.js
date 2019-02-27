4b6116c528f9ffb6eac1c92da7762d00392ef0df
AdrianMAD <46680955+AdrianMAD@users.noreply.github.com>
2019-02-27 22:03:57 +0200

    further improvements to the server functionality

--- src/App.js
+++ src/App.js
@@ -111,9 +111,9 @@ class App extends Component {
 
   loadUser = (data) => {
      this.setState({user: {
-      id: 'data.id',
-      name:'data.name',
-      email:'data.email',
+      id: data.id,
+      name: data.name,
+      email: data.email,
       entries: data.entries,
       joined: data.joined}})
   }
@@ -145,7 +145,21 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+         if(response){
+            fetch('http://localhost:3001/image',{
+               method:'put',
+               headers: {'Content-Type': 'application/json'},
+               body: JSON.stringify({
+                  id: this.state.user.id,
+			         })
+            })
+            .then(response => response.json())
+            .then(count => {
+               this.setState(Object.assign(this.state.user, {entries: count}))
+            })
+         }
+         this.displayFaceBox(this.calculateFaceLocation(response))})
       .catch(err => console.log(err))
     }
 
