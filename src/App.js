a34036c8220a1395ebcdcfedc779266ee20cf07d
AdrianMAD <46680955+AdrianMAD@users.noreply.github.com>
2019-03-12 13:37:23 +0200

    preparing for deployment

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ constructor() {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3001/imageurl', {
+    fetch('https://thawing-wildwood-75320.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -107,7 +107,7 @@ constructor() {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3001/image', {
+          fetch('https://thawing-wildwood-75320.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
